0551524f3f9c462e43a3f32c52699e551ea8c1ee
Numbericons <zachjoliver@gmail.com>
2020-11-26 23:58:37 -0800

    Add hand va

--- src/blackjack/player.jsx
+++ src/blackjack/player.jsx
@@ -7,19 +7,31 @@ function cardStyle(posX, posY){
   }
 }
 
+function cardArray(cards){
+  let arr = [];
+  for (let i=0; i<cards.length;i++) {
+    const card = cardStyle(cards[i].img_pos_x, cards[i].img_pos_y);
+    arr.push(<div className='card-show' style={card}>
+
+    </div>)
+  }
+  return arr;
+}
+
 export default function Player(props) {
-  const card1 = cardStyle(props.cards[0].img_pos_x, props.cards[0].img_pos_y);
-  const card2 = cardStyle(props.cards[1].img_pos_x, props.cards[1].img_pos_y);
+  // const card1 = cardStyle(props.cards[0].img_pos_x, props.cards[0].img_pos_y);
+  // const card2 = cardStyle(props.cards[1].img_pos_x, props.cards[1].img_pos_y);
+  const cards = cardArray(props.cards);
+  const showBust = props.total > 21 ? " Busted!" : "";
   return (
     <div className='hand'>
       <div className='hand-cards'>
-        <div className='card-show' style={card1}></div>
-        <div className='card-show' style={card2}></div>
+        {cards}
+        {/* <div className='card-show' style={card1}></div>
+        <div className='card-show' style={card2}></div> */}
       </div>
       <div className='hand-val'>
-        <div className='hand-val-txt'>{props.total}</div>
-        {/* <div className='hand-val-txt'>Flopped: {handRank[0].descr}</div>
-        <div className='hand-val-txt'>Overall: {handRank[1].descr}</div> */}
+        <div className='hand-val-txt'>{props.total}{showBust}</div>
       </div>
     </div>
   )
--- src/blackjack/table.jsx
+++ src/blackjack/table.jsx
@@ -53,10 +53,34 @@ export default class Table extends React.Component {
     this.setState({reset: true});
   }
 
+  cardVal(card) {
+    if (parseInt(card.rank)) return parseInt(card.rank);
+
+    const tens = ["T", "J", "Q", "K"];
+    if (tens.includes(card.rank)) return 10;
+    if (card.rank === "A") return 1;
+  }
+
+  handTotal(cards) {
+    let total = 0;
+    let aces = 0;
+
+    for (let i=0; i<cards.length; i++) {
+      if (cards[i].rank === "A") aces += 1;
+      total += this.cardVal(cards[i])
+    }
+    if (aces > 0 && total <= 11) total += 10; 
+
+    return total;
+  }
+
   showPlayers(){
-    return this.playerCards.map((cards, i) => (
-      <Player key={`player${i}`} cards={cards} board={this.boardCards}></Player>
-    ));
+    let playerArr = this.playerCards.map((cards, i) => {
+      const handTotal = this.handTotal(cards);
+      
+      return <Player key={`player${i}`} cards={cards} board={this.boardCards} total={handTotal}></Player>;
+    });
+    return playerArr;
   }
 
   refreshPage(){
